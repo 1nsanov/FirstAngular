09beb07f202d79fe09763dc3005751c4e563509c
moshinetsae <moshinetsae@mosreg.ru>
2023-03-15 14:07:56 +0200

    excel download

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -3,8 +3,12 @@ import {IProduct} from "../../api/models/product/Product";
 import {ProductService} from "../../services/product.service";
 import {ModalService} from "../../services/modal.service";
 import {ProductApiService} from "../../api/services/product.api.service";
+import {HttpClient} from "@angular/common/http";
 
-let test;
+
+interface IPassportDto{
+  file: string;
+}
 
 @Component({
   selector: 'app-product-page',
@@ -21,7 +25,8 @@ export class ProductPageComponent {
   constructor(
     private productService: ProductService,
     public modalService: ModalService,
-    private productApiService: ProductApiService) {
+    private productApiService: ProductApiService,
+    private http: HttpClient) {
   }
 
   ngOnInit(): void {
@@ -40,5 +45,35 @@ export class ProductPageComponent {
 
   switchLoad() {
     this.isLoad = !this.isLoad;
+  }
+
+  downloadExcel() {
+    this.http.get<IPassportDto>("http://localhost:5051/api/v1/passport/base", {
+      headers: {
+        'OrganizationId': '1',
+        'Region': 'Moscow',
+        'UserId': '1'
+      }
+    })
+      .subscribe(res => {
+        const bytes = res.file
+        const data = atob(bytes);
+        let array = new Array(data.length);
+        for (let i = 0; i < data.length; i++) {
+          array[i] = data.charCodeAt(i);
+        }
+
+        const dataUint8Array = new Uint8Array(array);
+        const blob = new Blob([dataUint8Array], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
+        const url = window.URL.createObjectURL(blob);
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = "Паспорт.xlsx";
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
   }
 }
